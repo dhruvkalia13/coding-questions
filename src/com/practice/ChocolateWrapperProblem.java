a48685d24b226af544c4de4dc60cbff3c776eb7a
Dhruv Kalia <dhruvkalia13@gmail.com>
2019-07-17 00:33:54 +0530

    FrequencySubstring

--- src/com/practice/ChocolateWrapperProblem.java
+++ src/com/practice/ChocolateWrapperProblem.java
@@ -5,9 +5,12 @@ public class ChocolateWrapperProblem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int money = 15;
-		int price = 1;
-		int wrap = 3;
+		/* 
+		 *how many chocolates can you eat? 
+		 */
+		int money = 15;	// money that you have
+		int price = 1;  //price of each chocolate
+		int wrap = 3;	// exchange of thiese many wrappers can get you one chocolate
 		
 		int choc = money/price;
 		int count = choc + (choc-1) / (wrap-1);
---
+++ src/com/practice/FrequencySubstring.java
@@ -0,0 +1,32 @@
+package com.practice;
+
+public class FrequencySubstring {
+/*
+ * Given a input string and a substring. Find the frequency of occurrences of substring in given string.
+ * 
+ * Input : man (pattern)
+        dhimanman (string)
+Output : 2
+
+Input : aa (pattern)
+        aaaaa (String)
+Output : 4
+
+*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "aa";
+		String input = "banana";
+		int count = 0;
+		int lengthStr = str.length();
+		for(int i = 0; i <= input.length() - lengthStr; i++) {
+			if((input.substring(i, i+lengthStr)).equals(str)) {
+				count++;
+			}
+		}
+		System.out.println(count);
+
+	}
+
+}
